63d55d1dab094c4521bc2984675eef47f4332770
sabbir268 <sabbir.hossain2668@gmail.com>
2020-12-06 13:43:53 +0600

    Last commit

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -19,14 +19,14 @@
           </div>
         </a>
         <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-          {{title}}
+          Traffic Analayzer
         </a>
       </div>
       <slot>
 
       </slot>
       <ul class="nav">
-        <!--By default vue-router adds an active class to each route link. This way the links are colored when clicked-->
+        
         <slot name="links">
           <sidebar-link v-for="(link,index) in sidebarLinks"
                         :key="index"
--- src/layout/dashboard/ContentFooter.vue
+++ src/layout/dashboard/ContentFooter.vue
@@ -3,37 +3,26 @@
     <div class="container-fluid">
       <ul class="nav">
         <li class="nav-item">
-          <a class="nav-link" href="http://www.creative-tim.com">
-            Creative Tim
-          </a>
+          <a class="nav-link" href="#"> Traffic Analyzer </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="http://blog.creative-tim.com">
-            Blog
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="http://www.creative-tim.com/license">
-            Licenses
-          </a>
+          <a class="nav-link" href="/"> Blog </a>
         </li>
       </ul>
       <div class="copyright">
-        © {{year}} made with <i class="tim-icons icon-heart-2"></i> by
-        <a href="https://www.binarcode.com" target="_blank" rel="noopener">Binar Code</a>&nbsp;&
-        <a href="https://www.creative-tim.com" target="_blank" rel="noopener">Creative Tim</a> for a better web.
+        © {{ year }}  Allrights reserved by TrafficAnalyzer.org
       </div>
     </div>
   </footer>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        year: new Date().getFullYear()
-      }
-    }
-  };
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
 </script>
 <style>
 </style>
